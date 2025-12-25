3f260e1e4672d9c21aa65e6986acfdc087202095
Gonroz <wasabaininja@gmail.com>
2025-12-25 01:09:35 -0500

    Finished Working with Databases
    Completely forgot that I was even using github for this. Just completed
    the working with databases section and it is working as it should. It's
    very late and I need to work so I'll be going to bed. But onto Routing
    and Structure next.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,87 @@
 use dioxus::prelude::*;
+use reqwest;
+// use rusqlite;
+use serde;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+static CSS: Asset = asset!("/assets/main.css");
+
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
 
 fn main() {
     dioxus::launch(App);
 }
 
-#[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! {
+        document::Stylesheet { href: CSS }
+        Title {}
+        DogView {}
+    }
 }
 
 #[component]
-pub fn Hero() -> Element {
+fn Title() -> Element {
     rsx! {
+        document::Stylesheet { href: CSS }
         div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+    // let mut img_src = use_resource(snipped!());
+
+    rsx! {
+        div { id: "dogview",
+            img { src: img_src.cloned().unwrap_or_default() }
+        }
+        div { id: "buttons",
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button {
+                id: "save",
+                onclick: move |_| async move {
+                    let current = img_src.cloned().unwrap();
+                    img_src.restart();
+                    _ = save_dog(current).await;
+                },
+                "save!"
             }
         }
     }
+}
+
+#[server]
+async fn save_dog(image: String) -> Result<()> {
+    DB.with(|f| f.execute("INSERT INTO dogs (url) VALUES (?1)", &[&image]))?;
+    Ok(())
+}
+
+#[cfg(feature = "server")]
+thread_local! {
+    pub static DB: rusqlite::Connection = {
+        let conn = rusqlite::Connection::open("hotdog.db").expect("Failed to open database.");
+
+        conn.execute_batch(
+            "CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                url TEXT NOT NULL
+            );"
+        ).unwrap();
+        conn
+    };
 }
